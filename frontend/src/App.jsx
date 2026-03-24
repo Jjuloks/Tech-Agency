89e9081d9bc18a79985e29ab99c6fde62be744ad
INFINITY\j.oksimowicz <j.oksimowicz@infinity-group.pl>
2026-03-24 17:09:38 +0100

    footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home/Home'
 import Services from './components/ServicesSection/ServicesSection'
 import Team from './components/TeamSection/TeamSection'
 import Post from './components/PostCard/Postcard'
+import Footer from './components/Footer/Footer'
 export default function App() {
   return (
     <>
@@ -15,7 +16,8 @@ export default function App() {
         <Route path="/team" element={<Team />} />
         <Route path="/posts" element={<Post />} />
       </Routes>
-
+      
+<Footer />
     </>
   )
 }
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      © 2026 Tech-Agency — Wszystkie prawa zastrzeżone
+    </footer>
+  )
+}
+
+export default Footer
