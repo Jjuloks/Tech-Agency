8f918dbb3d2a8ff1f2ea876bd1fe4f26ceb54218
INFINITY\j.oksimowicz <j.oksimowicz@infinity-group.pl>
2026-03-25 09:47:16 +0100

    new field imageService in serviceSection

--- frontend/src/components/ServiceCard/ServiceCard.jsx
+++ frontend/src/components/ServiceCard/ServiceCard.jsx
@@ -1,10 +1,10 @@
 import styles from './ServiceCard.module.css';
 import Image from '../../assets/web.jpeg'
 
-export default function ServiceCard({icon,title,description}){
+export default function ServiceCard({icon,title,description, imageServiceUrl}){
     return (
         <article className={styles.card}>
-            <img src={Image} className={styles.image} />
+            <img src={imageServiceUrl} className={styles.imageService} />
             <h3 className={styles.title}> {icon}{title}</h3>
             <p className={styles.desc}>{description}</p>
         </article>
