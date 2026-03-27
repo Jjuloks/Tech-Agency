9ccb9646d58415529dab2e5d0183fcd0d223e03b
INFINITY\j.oksimowicz <j.oksimowicz@infinity-group.pl>
2026-03-27 11:08:24 +0100

    layout for newsarticle

--- frontend/src/components/ArticlePage/ArticlePage.jsx
+++ frontend/src/components/ArticlePage/ArticlePage.jsx
@@ -2,11 +2,15 @@ import { useParams } from "react-router-dom";
 
 export default function ArticlePage() {
   const { slug } = useParams();
+  const {content } = useParams();
 
   return (
     <div>
       <h1>Artykuł: {slug}</h1>
       <p>Tu będzie zawartość artykułu.</p>
+        <div>
+            <p>{content} </p>
+        </div>
     </div>
   );
 }
