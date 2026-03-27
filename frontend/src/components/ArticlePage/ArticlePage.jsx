1e1f78962350208293a1a000a4b1e3a2e92e463e
INFINITY\j.oksimowicz <j.oksimowicz@infinity-group.pl>
2026-03-27 11:25:24 +0100

    fixed articleSinglePage

--- frontend/src/components/ArticlePage/ArticlePage.jsx
+++ frontend/src/components/ArticlePage/ArticlePage.jsx
@@ -1,16 +1,64 @@
 import { useParams } from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import styles from "./ArticlePage.module.css";
+ 
+function renderContent(content) {
+  if (Array.isArray(content)) {
+    return content.map((block, i) => {
+      if (block.type === "paragraph") {
+        return (
+          <p key={i}>
+            {block.children?.map((child) => child.text).join("")}
+          </p>
+        );
+      }
+      return null;
+    });
+  }
+ 
+  if (typeof content === "string") {
+    return <p>{content}</p>;
+  }
+ 
+  return null;
+}
+ 
 export default function ArticlePage() {
   const { slug } = useParams();
-  const {content } = useParams();
+  const [article, setArticle] = useState(null);
+ 
+  useEffect(() => {
+    fetch(
+      `http://localhost:1337/api/articles?filters[slug][$eq]=${slug}&populate=*`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("ARTICLE API:", data);
+        setArticle(data.data?.[0]);
+      });
+  }, [slug]);
+ 
+  if (!article) return <p>Ładowanie...</p>;
+ 
+  const data = article.attributes || article;
+ 
 
+  const imageUrl =
+    data.image?.url
+      ? `http://localhost:1337${data.image.url}`
+      : data.image?.data?.attributes?.url
+      ? `http://localhost:1337${data.image.data.attributes.url}`
+      : "";
+ 
   return (
-    <div>
-      <h1>Artykuł: {slug}</h1>
-      <p>Tu będzie zawartość artykułu.</p>
-        <div>
-            <p>{content} </p>
-        </div>
+    <div className={styles.container}>
+      <article className={styles.article}>
+        <h1>{data.title}</h1>
+ 
+        {imageUrl && <img src={imageUrl} alt={data.title} className={styles.image} />}
+        <div>{renderContent(data.content)}</div>
+      </article>
     </div>
   );
 }
+ 
--- frontend/src/components/NewsCard/NewsCard.jsx
+++ frontend/src/components/NewsCard/NewsCard.jsx
@@ -1,13 +1,14 @@
+import { useParams } from "react-router-dom";
 import styles from "./NewsCard.module.css";
 
 export default function NewsCard({ title, content,imageUrl, slug }) {
   return (
+
     <div className={styles.div}>
       <article className={styles.card}>
        <img src={imageUrl} alt={title} className={styles.image} />
 
         <h3 className={styles.title}>{title}</h3>
-
         <p className={styles.excerpt}>
           {typeof content === "string" ? content.slice(0, 120) : "Kliknij aby przeczytać"}
         </p>
